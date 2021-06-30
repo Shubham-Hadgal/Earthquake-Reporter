fc6ed54d363e01673fae71852dcb78e90d9cf075
Shubham Hadgal <shubhamhadgal45@gmail.com>
2021-06-30 11:28:38 +0530

    Fixed : showing wrong magnitude color for 0 index

--- app/src/main/java/com/example/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/quakereport/EarthquakeAdapter.java
@@ -122,6 +122,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         switch (magnitudeFloor)
         {
+            case 0:
             case 1:
                 magnitudeColorResourceId = R.color.magnitude1;
                 break;
